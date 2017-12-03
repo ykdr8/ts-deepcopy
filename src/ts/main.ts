1944d8c74e12de00e4bd522f11c8464724226236
TAK SNOW <taksnow@TAK-no-MacBook-Air.local>
2017-12-03 14:56:10 +0900

    Applied TSLint.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,9 +2,8 @@
  * Deepcopy function for TypeScript.
  * @param Tp Generic type of target/copied value.
  * @param tgt Target value to be copied.
- *
  */
-export default function TsDeepCopy<Tp>(tgt:Tp):Tp {
+export default function TsDeepCopy<Tp>(tgt: Tp): Tp {
 	return c.execute<Tp>(tgt);
 }
 /**
@@ -16,25 +15,24 @@ namespace c {
 	 * Executes deepcopy.
 	 * @param Tp Generic type of target/copied value.
 	 * @param tgt Target value to be copied.
-	 * 
 	 */
-	export function execute<Tp>(tgt:Tp):Tp {
-		let cp:Tp;
-		let ptn:number = 0;
+	export function execute<Tp>(tgt: Tp): Tp {
+		let cp: Tp;
+		let ptn: number = 0;
 		if (tgt === null) {
 			cp = tgt;
 		} else if (Array.isArray(tgt)) {
 			cp = [] as any;
 			(tgt as any[]).forEach((v, i, arr) => { (cp as any).push(v); });
-			(cp as any).forEach((n:any) => {
-				if ((typeof(n) === "object" && n !== {}) || Array.isArray(n)) {
+			(cp as any).forEach((n: any) => {
+				if ((typeof(n) === 'object' && n !== {}) || Array.isArray(n)) {
 					n = c.execute<any>(n);
 				}
 			});
-		} else if (typeof(tgt) === "object") {
+		} else if (typeof(tgt) === 'object') {
 			cp = { ...(tgt as Object) } as Tp;
 			Object.keys(cp).forEach(k => {
-				if (typeof((cp as any)[k]) === "object") {
+				if (typeof((cp as any)[k]) === 'object') {
 					(cp as any)[k] = c.execute<any>((cp as any)[k]);
 				}
 			});
--- test/src/main.ts
+++ test/src/main.ts
@@ -1,28 +1,28 @@
-import TsDeepCopy from "@src/ts/main";
+import TsDeepCopy from '@src/ts/main';
 
-import * as mocha from "mocha";
-import * as chai from "chai";
+import * as mocha from 'mocha';
+import * as chai from 'chai';
 
-function test():void {
-    const tgt = TsDeepCopy;
-	let master:c.I1 = c.obj;
-	let copy:c.I1 = TsDeepCopy<c.I1>(c.obj);
-	describe("@src/ts/main", function () {
-		describe("Deepcopied object\'s child value changed", function () {
+function test(): void {
+	const tgt = TsDeepCopy;
+	let master: c.I1 = c.obj;
+	let copy: c.I1 = TsDeepCopy<c.I1>(c.obj);
+	describe('@src/ts/main', () => {
+		describe('Deepcopied object\'s child value changed', () => {
 			master.a = 2;
-			it("should be different from master\'s.", function() {
+			it('should be different from master\'s.', () => {
 				chai.assert.notStrictEqual(master.a, copy.a);
 			});
 		});
-		describe("Deepcopied object\'s grandchild value changed", function () {
+		describe('Deepcopied object\'s grandchild value changed', () => {
 			master.i.a = 3;
-			it("should be different from master\'s.", function() {
+			it('should be different from master\'s.', () => {
 				chai.assert.notStrictEqual(master.i.a, copy.i.a);
 			});
 		});
-		describe("Deepcopied object\'s child function changed", function () {
-			master.h = function():string { return "hh" };
-			it("should be different from master\'s.", function() {
+		describe('Deepcopied object\'s child function changed', () => {
+			master.h = (): string => { return 'hh'; };
+			it('should be different from master\'s.', () => {
 				chai.assert.notStrictEqual(master.h(), copy.h());
 			});
 		});
@@ -44,7 +44,7 @@ namespace c {
 		f: boolean;
 		// Reference value
 		g: number[];
-		h():string;
+		h(): string;
 		i: {
 			a: number;
 			b: string;
@@ -53,36 +53,36 @@ namespace c {
 			e: number;
 			f: boolean;
 			g: number[];
-			h():string;
+			h(): string;
 			i: {
 				a: number;
 				b?: string;
 			};
 		};
 	}
-	export const obj:c.I1 = {
+	export const obj: c.I1 = {
 		// Primitive value
 		a: 1,
-		b: "b",
-		c: "",
+		b: 'b',
+		c: '',
 		d: null,
 		e: undefined,
 		f: true,
 		// Reference value
 		g: [1, 2, 3],
-		h: function () { return "h"; },
+		h: () => { return 'h'; },
 		i: {
 			a: 1,
-			b: "i.b",
-			c: "",
+			b: 'i.b',
+			c: '',
 			d: null,
 			e: undefined,
 			f: true,
 			g: [1, 2, 3],
-			h: function () { return "i.h"; },
-			i: { a: 1 }
-		}
-	}
+			h: () => { return 'i.h'; },
+			i: { a: 1 },
+		},
+	};
 }
 
 test();
